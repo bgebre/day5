e3f8159eb003130521b505e770a27b6d87ce39b5
Banna Gebre <bgebre09@gmail.com>
2014-07-25 19:13:53 +0000

    modified article and population

--- article.php
+++ article.php
@@ -3,7 +3,7 @@ $id =$_GET['id'];
 ?>
 
  <?php
- $connection = mysql_connect('localhost', 'root', "")
+ $connection = mysql_connect('localhost', 'root', "test54321")
   or die("Unable to connect to $localhost: " . mysql_errno());
 
  $selected_db = mysql_select_db('banna_log')
--- population.php
+++ population.php
@@ -1,6 +1,14 @@
-<h1>Welcome to My Articles</h1>
+<!DOCTYPE html>
+<html>
+  <head>
+    <link type="text/css" rel="stylesheet" href="stylesheet.css"/>
+    <title>Articles</title>
+  </head>
+  <body>
+    <h1>Blog Posts</h1>
+
 <?php
-$connection = mysql_connect('localhost', 'root', "")
+$connection = mysql_connect('localhost', 'root', 'test54321')
   or die("Unable to connect to $localhost: " . mysql_errno());
 
 $selected_db = mysql_select_db('banna_log')
@@ -10,9 +18,13 @@ $result = mysql_query("SELECT id, title, author, body FROM articles ORDER BY id 
 
 while ($row = mysql_fetch_array($result)){
  ?>
-  <h2><a href="http://33.33.33.40/day5/article.php?id=<?php print $row['id'];?>"><?php print $row['title'];?></a></h2>
-  <p><em>By:<?php print $row['author'];?></em></p>
-  <p><?php print $row['body'];?><p>
+  <div id="header">
+       <h2><a href="http://33.33.33.40/day5/article.php?id=<?php print $row['id'];?>"><?php print $row['title'];?></a></h2>
+     <div id= "body">
+       <p><em>By:<?php print $row['author'];?></em></p>
+       <p><?php print $row['body'];?></p>
+    </div>
+  </div>  
 <?php
 
 }
@@ -20,4 +32,4 @@ while ($row = mysql_fetch_array($result)){
 mysql_close($connection);
 ?>
 
-
+</html>
